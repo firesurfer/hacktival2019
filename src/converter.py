f20f7d710910e4d9d89df3160d6a3611339a8770
alexander <alexander.nitsch@posteo.de>
2019-05-12 00:34:30 +0200

    converter deals with money (untested)

--- src/converter.py
+++ src/converter.py
@@ -16,12 +16,10 @@ class converter:
         }
         self.length = [
             (0.1 * ureg.millimeter, "length", "{} * the width human hair"),
-            (1 * ureg.millimeter, None, None),
-            (1 * ureg.kilometer, None, None),
             (878.36 * ureg.kilometer, "length", "{} * the distance Paris -> Berlin"),
             (384400 * ureg.kilometer, "length", "{} * the distance to the moon")
         ]
-        self.money = [
+        self.money_comp = [
             (59039 * ureg.dollar, "dollar", "{} * the median annual US income", ),
             (20700000000 * ureg.dollar, "nasa", "{} * the NASA annual budget", ),
             (686074048000 * ureg.dollar, "military", "{} * the US military budget"),
@@ -36,47 +34,50 @@ class converter:
 
 
     def what_to_show(self,input, europe = True):
-        output = list()
+        output = []
         if type(input) is tuple:
-            conversion = " ".join(map(str, input))
-            output.append(tuple([conversion]))
-            if (europe and input[1] == "euro") or (not europe and input[1] == "dollar"):
-                # no conversion needed
-                output.append(None)
+            inputstring = " ".join(map(str, input))
+            output.append((inputstring, input[1]))
+            dollarvalue = self.convert_currency(input[0], input[1], "dollar")
+            if europe:
+                converted = (self.convert_currency(input[0], input[1], "euro"), "euro")
             else:
-                output_eur, output_dol = self.convert_currency(input, europe)
-                output.append
+                converted = (dollarvalue, "dollar")
+
+            convertedstring = " ".join(map(str, converted))
+            output.append((convertedstring, converted[1]))
+            compstrings = self.relate_to(dollarvalue, self.money_comp)
+            for comp in compstrings:
+                output.append(comp)
+
+        return output
 
 
 
-
-
-
-
-
-
-    def convert(self, input, europe = True):
-        if type(input) is tuple:
-            return self.convert_currency(input, europe)
-        else:
-            return self.convert_unit(input, europe)
-
-    def convert_currency(self, input, europe:bool):
+    def convert_currency(self, value, source_currency, goal_currency):
 
         conv = self.money_conversion
-        output_eur = (conv["euro"] / conv[input[1]]) * input[0]
-        output_dol = (conv["dollar"] / conv[input[1]]) * input[0]
-        if europe:
-            return output_eur, self.relate_money(output_dol)
-        else:
-            return output_dol, self.relate_money(output_dol)
+        return (conv[goal_currency] / conv[source_currency]) * value
+
+    def relate_to(self, value, comp_list): # expects value in Dollars
+        comp_with_factors = list()
+        for comp in comp_list:
+            factor = value / comp[0]
+            comp_with_factors.append((comp, factor))
+        filtered_comp_with_factors = list(filter(lambda x: 0.01 <= x[1] <= 1000, comp_with_factors))
+        if len(filtered_comp_with_factors) > 2:
+            filtered_comp_with_factors = [filtered_comp_with_factors[0], filtered_comp_with_factors[-1]]
+        compstrings = []
+        for element in filtered_comp_with_factors:
+            showstring = element[0][2].format(element[1])
+            icon = element[0][1]
+            compstrings.append((showstring, icon))
+        return compstrings
 
 
-    def relate_money(self, input): # expects value in Dollars
-        value = input[0]
-        factors = list()
-        for comp in self.money:
-            factors.append((math.log10(value / comp[0]) - 1)**2)
+
+
+
         sortedfactors = deepcopy(factors)
         sortedfactors.sort()
         index1 = factors.index(sortedfactors[0])
