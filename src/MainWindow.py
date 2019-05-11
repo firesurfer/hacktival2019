31a7bd213b5d3ffe3d9eea1d1d123ba13c8c43ef
Lennart Nachtigall <lennart.nachtigall@student.kit.edu>
2019-05-11 12:15:04 +0200

    we've got a main window

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -4,9 +4,10 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
-        super().__init()
-        self.initUi()
+        super().__init__()
+        self.initUI()
     
     def initUI(self):
-        
+        pass
