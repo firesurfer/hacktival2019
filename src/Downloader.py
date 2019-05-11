2807255e17de1dfa65c3c4e713ec95fdf852fb93
alexander <alexander.nitsch@posteo.de>
2019-05-11 15:32:04 +0200

    Improved path joining

--- src/Downloader.py
+++ src/Downloader.py
@@ -10,14 +10,15 @@ from youtube_transcript_api import YouTubeTranscriptApi
 def get_vid_and_sub_path(url):
     video_id = url[url.find("v=")+2:]  # everything from the url from v= onwards
     dl_path = Path("../dl/" + video_id + "/")
-    sub_path = os.path.join(dl_path, Path("sub.dmp"))
-    vid_path = os.path.join(dl_path, Path("vid.mp4"))
+    sub_path = dl_path / Path("sub.dmp")
+    vid_path = dl_path / Path("vid.mp4")
 
     if not Path.exists(dl_path):
         os.makedirs(dl_path)
         YouTube(url).streams.first().download(dl_path, "vid")
         sub = YouTubeTranscriptApi.get_transcripts([video_id], languages=['en'])
         pickle.dump(sub, open(sub_path, "wb"))
+        # To load:
         # sub = pickle.load(open(sub_path, "rb"))
 
     return sub_path, vid_path
