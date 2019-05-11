cdbc4961ceaf9f1861f08388933d2668d3af6826
alexander <alexander.nitsch@posteo.de>
2019-05-11 15:28:45 +0200

    Downloading both subs and vid works (most of the time)

--- src/Downloader.py
+++ src/Downloader.py
@@ -1,20 +1,27 @@
 # Downloads the video and subtitles
 from pathlib import Path
 import os
+import pickle
 
 from pytube import YouTube
-import youtube_transcript_api
-
-url = "https://www.youtube.com/watch?v=n06H7OcPd-g"
-
-video_id = url[url.find("v=")+2:]  # everything from the url from v= onwards
-print(video_id)
-dl_path = Path("../dl/" + video_id + "/")
-if not Path.exists(dl_path):
-    os.makedirs(dl_path)
-    YouTube(url).streams.first().download(dl_path)  # Source : https://stackoverflow.com/questions/40713268
-else:
-    pass  # Todo: check if video and subtitle file is there (?)
+from youtube_transcript_api import YouTubeTranscriptApi
 
 
+def get_vid_and_sub_path(url):
+    video_id = url[url.find("v=")+2:]  # everything from the url from v= onwards
+    dl_path = Path("../dl/" + video_id + "/")
+    sub_path = os.path.join(dl_path, Path("sub.dmp"))
+    vid_path = os.path.join(dl_path, Path("vid.mp4"))
 
+    if not Path.exists(dl_path):
+        os.makedirs(dl_path)
+        YouTube(url).streams.first().download(dl_path, "vid")
+        sub = YouTubeTranscriptApi.get_transcripts([video_id], languages=['en'])
+        pickle.dump(sub, open(sub_path, "wb"))
+        # sub = pickle.load(open(sub_path, "rb"))
+
+    return sub_path, vid_path
+
+
+if __name__ == "__main__":
+    get_vid_and_sub_path("https://www.youtube.com/watch?v=RUVs_5mbJSA")
