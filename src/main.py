62397df71845854ff933ecf48c5028629abbd572
alexander <alexander.nitsch@posteo.de>
2019-05-11 12:44:38 +0200

    Parse input params (video and subtitle file path)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import sys
+import argparse
+from pathlib import Path
+
 
 from PyQt5.QtWidgets import  QApplication
 from MainWindow import MainWindow
@@ -7,8 +10,17 @@ from MainWindow import MainWindow
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
+
+    # Parse input parameters (video and subtitle file)
+    parser = argparse.ArgumentParser(description='Understanding units and costs better')
+    parser.add_argument('videofilename')
+    parser.add_argument('subtitlefilename')
+    args = parser.parse_args()
+    videopath = Path(args.videofilename)
+    subpath = Path(args.subtitlefilename)
+    if not videopath.is_file() or not subpath.is_file():
+        raise Exception
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-    
- 
